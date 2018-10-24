2382aa07d8e31613808afb459ba5dc1c982e32b6
Teo Espero <tespero@wgu.edu>
2018-10-24 16:31:06 -0700

    created librares module

---
+++ private/shared/staff-header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="../stylesheets/normalize.css" rel="stylesheet" type="text/css">
+    <link href="../stylesheets/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teodulfo Espero | Staff Area - Globe Bank CMS Project</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>Globe Bank CMS - Staff Area</h1>
+        </div>
+    </header>
+
+    <nav>
+            <ul>
+                <li><a href="index.php">Menu</a></li>
+            </ul>
+    </nav>
--- staff/index.php
+++ staff/index.php
@@ -6,40 +6,14 @@
     #Web-Dev-Bootcamp-Lynda
 -->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link href="../stylesheets/normalize.css" rel="stylesheet" type="text/css">
-    <link href="../stylesheets/style.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teodulfo Espero | Globe Bank CMS Project</title>
-</head>
-<body>
-    <header>
-        <div class="container">
-            <h1>Globe Bank CMS - Staff Area</h1>
-        </div>
-    </header>
 
-    <nav>
-            <ul>
-                <li><a href="index.php">Menu</a></li>
-            </ul>
-    </nav>
 
+ <!-- load the initialize file to load required functions -->
+<?php require_once('../private/initialize.php'); ?>
+
+<?php include('../private/shared/staff-header.php'); ?>
+    
     <div id="content">
     </div>
 
-    <footer>
-        &copy; <?php echo date('Y'); ?> Globe Bank International
-    </footer>
-
-    <!-- required JS files -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include('../private/shared/staff-footer.php'); ?>
